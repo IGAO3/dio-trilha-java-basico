d9bd2556899dfef8f7d0ea27d1f9cf0eeb9a8c2c
Igor Adriano <igor.cordeiro3@hotmail.com>
2024-05-16 22:29:31 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
+        Scanner sc = new Scanner(System.in);
+
+        String nome = "Igor Adriano Cordeiro de Matos";
+
+        double saldo = 237.48;
+
+        System.out.println("Plataforma Banco X");
+        
+        System.out.println("Digite o número da agência: ");
+        String agencia = sc.next();
+
+        System.out.println("Digite o número da conta:" );
+        int conta = sc.nextInt();
+
+        System.out.println("Olá " + nome + "! Obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque!");
+
+
+
+
     }
 }
